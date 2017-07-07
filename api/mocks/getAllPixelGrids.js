f0f079687aa6c10d6cb7176867706fd12cbfe7f7
Charlie Greenman <charie@razroo.com>
2017-07-06 21:55:05 -0400

    Update the swagger mock for get requests.

--- api/mocks/getAllPixelGrids.js
+++ api/mocks/getAllPixelGrids.js
@@ -1,14 +1,24 @@
 'use strict';
 
 module.exports = {
-  getAllPixelGrids: getAllPixelGrids
+  GetAllPixelGrids: GetAllPixelGrids
 }
 
-function getAllPixelGrids(req, res, next) {
+function GetAllPixelGrids(req, res, next) {
   res.json([
     {
       grid_id: 0,
-
+      grid: "super mario",
+      datecreated: "2017-07-07T01:20:05+00:00",
+      author: "charlie",
+      completed: false
+    },
+    {
+      grid_id: 1,
+      grid: "donkey kong",
+      datecreated: "2017-07-11T01:20:05+00:00",
+      author: "mathew",
+      completed: false
     }
   ])
 }
